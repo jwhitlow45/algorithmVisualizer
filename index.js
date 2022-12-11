9d9c57be494d7aef96fe6e7548b122587f93a650
jwhitlow45 <jwhitlow45@gmail.com>
2022-12-11 02:57:36 -0800

    feat:add toolbar

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const display_parameter_submit = document.getElementById('display-parameter-submit');
 display_parameter_submit.addEventListener('click',  buildDisplay);
 
+const add_wall_tool = document.getElementById('add-wall-tool');
+const remove_wall_tool = document.getElementById('remove-wall-tool');
+const set_start_tool = document.getElementById('set-start-tool');
+
+set_start_tool.addEventListener('click', function(){ selectTool('set-start-tool') });
+add_wall_tool.addEventListener('click', function(){ selectTool('add-wall-tool') });
+remove_wall_tool.addEventListener('click', function(){ selectTool('remove-wall-tool') });
+
 function buildDisplay() {
   const display_parameter_form = document.getElementById('display-parameter-form');
   const formData = new FormData(display_parameter_form);
@@ -33,9 +41,17 @@ function selectTool(tool_name) {
   const TOOLS = {
     'set-start-tool' : 0,
     'add-wall-tool' : 1,
-    'remove-call-tool' : 2,
+    'remove-wall-tool' : 2,
   };
   selected_tool = TOOLS[tool_name];
+
+  const tool_elements = document.getElementsByClassName('tool');
+  for (let tool_elem of tool_elements) {
+    if (tool_elem.id == tool_name)
+      tool_elem.style.opacity = .5;
+    else
+      tool_elem.style.opacity = 1;
+  }
 }
 
 buildDisplay();
